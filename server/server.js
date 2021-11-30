86267928feefdcae50bd1e8f5a58e77ddf959a9b
830503 <lixiaoqi19830503@gmail.com>
2021-11-30 20:41:53 +0100

    new friend can be added to the serve

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.all("/*", function(req, res, next){
 });
 
 
-let allFriends = [{fName: 'Coach', lName: 'Tim', email: 'tim.broos@becode.org', phone: '0469420666', signatureMove: 'Yeet', language: 'Javascript'}];
+let allFriends = [{firstName: 'Coach', lastName: 'Tim', email: 'tim.broos@becode.org', telephone: '0469420666', signatureMove: 'Yeet', languages: 'Javascript'}];
 
 // Below you can define how your API handles a get or a post request.
 // Try sending a get request to the root, you should get a "Hello from server" back.
@@ -31,10 +31,12 @@ app.get('/allFriends', function(request, response) {
 
 app.post('/', function (request, response) {
     response.status(200).send({"message": "Data received"});
+    
 });
 
-app.post('/addFriends', function(request, response) {
+app.post('/addFriend', function(request, response) {
     allFriends.push(request.body);
-})
+    // console.log(request.body);
+});
 
 app.listen(PORT, function () {});
