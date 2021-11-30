f2164e6f55f8347a97df2e39b31dc055de77c536
830503 <lixiaoqi19830503@gmail.com>
2021-11-30 10:56:57 +0100

    data bingding succesful

--- friendform/src/app/friend.ts
+++ friendform/src/app/friend.ts
@@ -5,7 +5,13 @@ export class Friend {
     telephone: number;
     language: string;
 
-    public constructor(firstName: string, lastName: string, email: string, telephone: number, language: string){
+    public constructor(
+        firstName: string,
+        lastName: string,
+        email: string,
+        telephone: number,
+        language: string
+    ) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
